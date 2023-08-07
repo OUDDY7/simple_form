055f57866e61c290415fa30fd7e568d264560b2e
OUDDY7 <141331165+OUDDY7@users.noreply.github.com>
2023-08-07 11:47:00 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("hello world")
+
+
+const arr=[1]
+
+const valueReturnedfrompush=arr.push(8);
+
+console.log(valueReturnedfrompush)
+
+ //const valueReturnedfrompop=arr.pop();
+
+// console.log(valueReturnedfrompop)
+ const valueReturnedfromunshift=arr.unshift(0);
+
+ //console.log(valueReturnedfromunshift)
+
+//const valueReturnedfromshift=arr.shift();
+
+//console.log(valueReturnedfromshift)
+
+const arr2=[3,2,1]
+
+const valueconc=arr.concat(arr2)
+
+console.log(valueconc)
+
+
+
